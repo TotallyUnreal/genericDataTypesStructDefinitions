104f7a6c63d38dab974d73a83c648206af40aefd
False N. Name <Mstow@protonmail.com>
2024-06-05 14:11:08 -0500

    implement trait for point struct using generics and display integer and float values

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,21 @@ struct PointMuliGenerics<T, U> {
     y: U,
 }
 
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
 fn main() {
     let integer = Point { x: 5, y: 10 };
     let float = Point { x: 10.0, y: 19.9 };
     //    let wont_work = Point { x: 25, y: 54.5 } // Needs to be the same datatype if adding
     // data to same generic type
     let will_work = PointMuliGenerics { x: 6.4, y: 5 };
+    println!(
+        "integer point x: {}, float point x: {}",
+        integer.x(),
+        float.x()
+    );
 }
