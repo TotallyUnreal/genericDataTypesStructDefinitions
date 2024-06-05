05e157568c8ac2e2dfccd0ce6eacc8695bab9a66
False N. Name <Mstow@protonmail.com>
2024-06-05 14:06:45 -0500

    Add new Point struct with multiple generic types and showcase how it now allows a int and float to define a point location

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,15 @@ struct Point<T> {
     y: T,
 }
 
+struct PointMuliGenerics<T, U> {
+    x: T,
+    y: U,
+}
+
 fn main() {
     let integer = Point { x: 5, y: 10 };
     let float = Point { x: 10.0, y: 19.9 };
     //    let wont_work = Point { x: 25, y: 54.5 } // Needs to be the same datatype if adding
     // data to same generic type
+    let will_work = PointMuliGenerics { x: 6.4, y: 5 };
 }
