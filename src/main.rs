4fa60257bea48f1ab7e7865f46622bbbd3bd8151
False N. Name <Mstow@protonmail.com>
2024-06-05 13:51:43 -0500

    Add Point struct and create examples to show how generics work in rust structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 10.0, y: 19.9 };
+    //    let wont_work = Point { x: 25, y: 54.5 } // Needs to be the same datatype if adding
+    // data to same generic type
 }
